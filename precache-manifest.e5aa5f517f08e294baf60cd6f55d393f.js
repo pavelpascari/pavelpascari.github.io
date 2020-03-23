7eaee30fcf173d6180c34d3f2c3dfb4d4a263fbb
Pavel P <pascaripavel@gmail.com>
2020-03-23 23:23:56 +0100

    deploy

---
+++ precache-manifest.e5aa5f517f08e294baf60cd6f55d393f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415c1574a391094136c7",
+    "url": "/pavelpascari.github.io/css/app.3349495a.css"
+  },
+  {
+    "revision": "bc2de304e7e7a971ef0c286b3ef12d38",
+    "url": "/pavelpascari.github.io/index.html"
+  },
+  {
+    "revision": "261f78aae49181c83b33",
+    "url": "/pavelpascari.github.io/js/about.bdfd6e30.js"
+  },
+  {
+    "revision": "415c1574a391094136c7",
+    "url": "/pavelpascari.github.io/js/app.5962a1a5.js"
+  },
+  {
+    "revision": "fbf639e1deb7df77a467",
+    "url": "/pavelpascari.github.io/js/chunk-vendors.83bea35d.js"
+  },
+  {
+    "revision": "5f54c2d3b6e1657fb5339a69530de8ad",
+    "url": "/pavelpascari.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pavelpascari.github.io/robots.txt"
+  }
+]);

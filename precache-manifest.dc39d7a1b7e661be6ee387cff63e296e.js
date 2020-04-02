a934f658e817439815291d2192d3183641b8e2c8
Pavel P <pascaripavel@gmail.com>
2020-04-02 23:32:43 +0200

    deploy

---
+++ precache-manifest.dc39d7a1b7e661be6ee387cff63e296e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb907c3c28f1f9564d4",
+    "url": "/css/app.af6c081a.css"
+  },
+  {
+    "revision": "07c59784840b0b57620237035329c927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb907c3c28f1f9564d4",
+    "url": "/js/app.5acc599b.js"
+  },
+  {
+    "revision": "780d9042b94dba713983",
+    "url": "/js/chunk-vendors.5799dee5.js"
+  },
+  {
+    "revision": "5f54c2d3b6e1657fb5339a69530de8ad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
